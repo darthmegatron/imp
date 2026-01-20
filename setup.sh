4f1caddae0372eb61747b0131398b53d9cdf2bae
Robert Collins <randomhuman-rc@proton.me>
2026-01-20 10:52:06 -0500

    Structure update

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env zsh
 
 python3 -m venv venv
-pip3 install -r requirements.txt
 source venv/bin/activate
+pip3 install -r requirements.txt
 
 clear && echo "To exit the virtual environment use the command 'deactivate'"
 
